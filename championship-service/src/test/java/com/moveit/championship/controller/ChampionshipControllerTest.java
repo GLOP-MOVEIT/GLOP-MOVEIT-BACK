534e43dd0c33dff807ccfa91095e5f2e1dfcb006
CamilleBarthelemy <camille.barthelemy59@gmail.com>
2026-01-19 16:36:48 +0100

    fix: testing

--- championship-service/src/test/java/com/moveit/championship/controller/ChampionshipControllerTest.java
+++ championship-service/src/test/java/com/moveit/championship/controller/ChampionshipControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ChampionshipController.class)
+@Import(ObjectMapper.class)
 public class ChampionshipControllerTest {
 
     @Autowired
