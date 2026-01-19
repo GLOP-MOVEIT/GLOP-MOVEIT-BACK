26551cb941ddb47f8b9c9bd9704dce8a616541d0
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2026-01-19 14:54:54 +0100

    wip: rename AuthService to UserService and clean up unused authentication-related code

--- user-service/src/main/java/com/moveit/user/controller/UserController.java
+++ user-service/src/main/java/com/moveit/user/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.moveit.user.controller;
 
 import com.moveit.auth.entity.User;
-import com.moveit.auth.model.RegisterUserDto;
-import com.moveit.auth.service.UserService;
+import com.moveit.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,11 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,24 +18,11 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth/users")
+@RequestMapping("/users")
 @Tag(name = "Utilisateurs", description = "API de gestion des utilisateurs")
 public class UserController {
 
     private final UserService userService;
-
-    @Operation(summary = "Récupération de l'utilisateur authentifié")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Récupération de l'utilisateur authentifié avec succès", content = @Content(schema = @Schema(implementation = User.class))),
-            @ApiResponse(responseCode = "401", description = "Utilisateur non authentifié", content = @Content()),
-            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur", content = @Content())
-    })
-    @GetMapping("/me")
-    @PreAuthorize("isAuthenticated()")
-    public User authenticatedUser(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return (User) authentication.getPrincipal();
-    }
 
     @Operation(summary = "Récupération de tous les utilisateurs")
     @ApiResponses(value = {
@@ -52,18 +34,5 @@ public class UserController {
     @PreAuthorize("hasAnyRole('ADMIN', 'COMMISSIONER')")
     public List<User> allUsers() {
         return userService.allUsers();
-    }
-
-    @Operation(summary = "Création d'un administrateur")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Administrateur créé avec succès", content = @Content(schema = @Schema(implementation = User.class))),
-            @ApiResponse(responseCode = "400", description = "Données d'inscription invalides", content = @Content()),
-            @ApiResponse(responseCode = "409", description = "L'utilisateur existe déjà", content = @Content()),
-            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur", content = @Content())
-    })
-    @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
-    public User createAdministrator(@RequestBody RegisterUserDto registerUserDto) {
-        return userService.createAdministrator(registerUserDto);
     }
 }
