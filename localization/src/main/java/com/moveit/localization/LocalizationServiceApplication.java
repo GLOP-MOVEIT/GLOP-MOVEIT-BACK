4e2717a30673da9fca74c7e152d38f6a448815ef
Evann <evann59167@gmail.com>
2026-01-25 20:45:34 +0100

    feat: add initial localization service with Maven setup and application structure

---
+++ localization/src/main/java/com/moveit/localization/LocalizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moveit.localization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizationServiceApplication.class, args);
+	}
+
+}
