7b5a8fad661d2f93092262b87a2b7de091f8998b
Evann <evann59167@gmail.com>
2026-01-25 21:34:12 +0100

    feat: add LocationResponse and LocationUpdateRequest DTOs with validation annotations

---
+++ localization/src/main/java/com/moveit/localization/dto/LocationResponse.java
@@ -0,0 +1,20 @@
+package com.moveit.localization.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationResponse {
+
+    private Integer userId;
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime updatedAt;
+}
