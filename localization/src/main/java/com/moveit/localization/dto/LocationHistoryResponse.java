492ad9a4be4d4374bd83ec4a6fb84a969257065f
Evann <evann59167@gmail.com>
2026-01-25 22:20:38 +0100

    feat: add LocationHistoryResponse DTO for location history data

---
+++ localization/src/main/java/com/moveit/localization/dto/LocationHistoryResponse.java
@@ -0,0 +1,21 @@
+package com.moveit.localization.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationHistoryResponse {
+
+    private Long id;
+    private Integer userId;
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime timestamp;
+}
