dac169512cff031e1c4ec013f360d6d5a22550c8
evann.lietard <evann.lietard.etu@univ-lille.fr>
2026-02-02 14:21:32 +0100

    feat: initialize volunteer service with Maven setup and basic application structure

---
+++ volunteer-service/src/test/java/com/moveit/volunteer_service/VolunteerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveit.volunteer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
