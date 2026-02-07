a4b320c48a0fe6bfcd6a596d83215b231d02aa88
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2026-02-07 20:19:07 +0100

    fix: update getRoleByName method to use PathVariable and rename id to roleId

--- user-service/src/main/java/com/moveit/user/controller/RoleController.java
+++ user-service/src/main/java/com/moveit/user/controller/RoleController.java
@@ -4,10 +4,7 @@ import com.moveit.user.dto.Role;
 import com.moveit.user.service.RoleService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class RoleController {
     }
 
     @GetMapping("/{name}")
-    public Role getRoleByName(@RequestParam String name) {
+    public Role getRoleByName(@PathVariable String name) {
         return this.roleService.getRoleByName(name);
     }
 }
