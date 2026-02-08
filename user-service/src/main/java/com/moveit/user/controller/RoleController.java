91f9ca96a9265cc53a67ad56245ac35bd4146681
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2026-02-08 22:33:21 +0100

    feat: add OpenAPI annotations for user, role, and request controllers

--- user-service/src/main/java/com/moveit/user/controller/RoleController.java
+++ user-service/src/main/java/com/moveit/user/controller/RoleController.java
@@ -2,6 +2,11 @@ package com.moveit.user.controller;
 
 import com.moveit.user.dto.Role;
 import com.moveit.user.service.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +21,11 @@ public class RoleController {
 
     private final RoleService roleService;
 
+    @Operation(summary = "Récupérer tous les rôles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rôles récupérés avec succès", content = @Content(schema = @Schema(implementation = Role.class))),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur", content = @Content())
+    })
     @GetMapping
     public List<Role> getAllRoles() {
         return this.roleService.getAllRole();
