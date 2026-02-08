9405d1dfe002227c1e8726ab22c43e8f3572f1e0
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2026-02-08 12:04:36 +0100

    feat: update UserController and UserService to use UserRequest DTO with validation

--- user-service/src/main/java/com/moveit/user/mapper/UserMapper.java
+++ user-service/src/main/java/com/moveit/user/mapper/UserMapper.java
@@ -1,12 +1,26 @@
 package com.moveit.user.mapper;
 
 import com.moveit.user.dto.User;
+import com.moveit.user.dto.UserRequest;
+import com.moveit.user.entity.RoleEntity;
 import com.moveit.user.entity.UserEntity;
+import com.moveit.user.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+@RequiredArgsConstructor
+public abstract class UserMapper {
 
-    UserEntity toEntity(User user);
-    User toDto(UserEntity userEntity);
+    private final RoleRepository roleRepository;
+
+    public abstract User toDto(UserEntity userEntity);
+
+    @Mapping(target = "role", expression = "java(getSpectatorRole())")
+    public abstract UserEntity toEntity(UserRequest userRequest);
+
+    private RoleEntity getSpectatorRole() {
+        return this.roleRepository.findByName("SPECTATOR").get();
+    }
 }
