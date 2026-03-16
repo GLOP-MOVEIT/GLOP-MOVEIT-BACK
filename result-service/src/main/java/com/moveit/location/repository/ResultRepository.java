eb441b5f0a172330b7f84697b3d24b40da2bab90
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2026-03-16 15:17:26 +0100

    feat: implement ResultController, ResultService, and ResultRepository for managing results

---
+++ result-service/src/main/java/com/moveit/location/repository/ResultRepository.java
@@ -0,0 +1,19 @@
+package com.moveit.location.repository;
+
+import com.moveit.location.entity.ResultEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ResultRepository extends JpaRepository<ResultEntity, Integer> {
+
+    Optional<ResultEntity> findByTrialId(Integer trialId);
+
+    @Query("SELECT r FROM ResultEntity r JOIN r.rankings rk WHERE rk.id = :rankingId")
+    List<ResultEntity> findByParticipantsId(@Param("rankingId") Integer rankingId);
+}
