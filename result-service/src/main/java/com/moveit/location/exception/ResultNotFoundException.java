a5fab9304a4464e33617b520072e99b785e3c62f
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2026-03-16 15:17:26 +0100

    feat: implement ResultController, ResultService, and ResultRepository for managing results

---
+++ result-service/src/main/java/com/moveit/location/exception/ResultNotFoundException.java
@@ -0,0 +1,8 @@
+package com.moveit.location.exception;
+
+public class ResultNotFoundException extends RuntimeException {
+
+    public ResultNotFoundException(String message) {
+        super(message);
+    }
+}
