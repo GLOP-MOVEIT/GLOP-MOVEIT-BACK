41e8302997f4a4019fcbb40c182b3515e15441e0
Julien-Conoir <julien.conoir.etu@univ-lille.fr>
2026-03-20 11:43:57 +0100

    fix: remove files in wrong package structure

--- result-service/src/main/java/com/moveit/location/exception/ResultNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.moveit.location.exception;
-
-public class ResultNotFoundException extends RuntimeException {
-
-    public ResultNotFoundException(String message) {
-        super(message);
-    }
-}
